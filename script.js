d228902286033dbec05bf737b1585dac5dfcfdfe
Ramiz Afzal <m.ramiz.afzal@gmail.com>
2024-05-10 17:43:32 +0500

    updated main script, completed TODOs

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import pluginStealth from 'puppeteer-extra-plugin-stealth';
+import { executablePath } from 'puppeteer';
 import dotenv from 'dotenv';
 import { stringify } from 'csv-stringify';
 import fs from 'fs';
@@ -7,92 +9,109 @@ import fs from 'fs';
 dotenv.config();
 
 (async () => {
+	// output file name
+	let outPutDir = './results';
+	let fileName = `${Date.now()}-data.csv`;
+	let outPutFileName = `${outPutDir}/${fileName}`;
 
-    // create & open browser
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(process.env.SITEMAP_ROOT_URL);
-    await page.setViewport({ width: 1080, height: 1024 });
+	let userAgentHeaders = {
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+		'upgrade-insecure-requests': '1',
+		accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+		'accept-encoding': 'gzip, deflate, br',
+		'accept-language': 'en-US,en;q=0.9,en;q=0.8',
+	};
 
-    await page.waitForSelector('loc');
-    const elements = await page.$$('loc');
+	// create & open browser
+	puppeteer.use(pluginStealth());
+	const browser = await puppeteer.launch({ headless: false, executablePath: executablePath() });
+	const page = await browser.newPage();
+	await page.setExtraHTTPHeaders(userAgentHeaders);
+	await page.goto(process.env.SITEMAP_ROOT_URL);
+	await page.setViewport({ width: 1080, height: 1024 });
 
-    const archiveURLs = [];
-    for (const element of elements) {
-        const text = await page.evaluate(el => el.textContent, element);
-        archiveURLs.push(text);
-    }
+	await page.waitForSelector('loc');
+	const elements = await page.$$('loc');
 
-    if (archiveURLs.length == 0) {
-        await browser.close();
-        console.log('No article archive URLs were found on this sitemap URL')
-        return;
-    }
+	const archiveURLs = [];
+	for (const element of elements) {
+		const text = await page.evaluate((el) => el.textContent, element);
+		archiveURLs.push(text);
+	}
 
-    let articleArchiveURLs = [];
-    for (const archiveURL of archiveURLs) {
-        if (archiveURL.includes(process.env.MATCH_CONTENT)) {
-            articleArchiveURLs.push(archiveURL);
-        }
-    }
+	if (archiveURLs.length == 0) {
+		await browser.close();
+		console.log('No article archive URLs were found on this sitemap URL');
+		return;
+	}
 
-    if (articleArchiveURLs.length == 0) {
-        await browser.close();
-        console.log('Filter returned 0 article archive URLs for this sitemap URL')
-        return;
-    }
+	let articleArchiveURLs = [];
+	for (const archiveURL of archiveURLs) {
+		if (archiveURL.includes(process.env.MATCH_CONTENT)) {
+			articleArchiveURLs.push(archiveURL);
+		}
+	}
 
-    let articleURLs = [];
-    for (const archiveURL of articleArchiveURLs) {
-        const _page = await browser.newPage();
-        await _page.goto(archiveURL);
-        await _page.setViewport({ width: 1080, height: 1024 });
-        await _page.waitForTimeout(3000);
-        await _page.waitForSelector('loc');
-        const _elements = await _page.$$('loc');
+	if (articleArchiveURLs.length == 0) {
+		await browser.close();
+		console.log('Filter returned 0 article archive URLs for this sitemap URL');
+		return;
+	}
 
-        const postURLs = [];
-        for (const element of _elements) {
-            const text = await _page.evaluate(el => el.textContent, element);
-            postURLs.push(text);
-        }
+	let articleURLs = [];
+	for (const archiveURL of articleArchiveURLs) {
+		const _page = await browser.newPage();
+		await _page.setExtraHTTPHeaders(userAgentHeaders);
+		await _page.goto(archiveURL);
+		await _page.setViewport({ width: 1080, height: 1024 });
+		await _page.waitForTimeout(10000);
+		await _page.waitForSelector('loc');
+		const _elements = await _page.$$('loc');
 
-        if (postURLs.length == 0) {
-            console.log(`Got empty response, URL: ${archiveURL}`)
-            continue;
-        }
+		const postURLs = [];
+		for (const element of _elements) {
+			const text = await _page.evaluate((el) => el.textContent, element);
+			postURLs.push(text);
+		}
 
-        articleURLs.concat(postURLs);
-        await _page.close();
-    }
+		if (postURLs.length == 0) {
+			console.log(`Got empty response, URL: ${archiveURL}`);
+			continue;
+		}
 
-    if (articleURLs.length == 0) {
-        await browser.close();
-        console.log('Filter returned 0 article URLs for this sitemap URL')
-        return;
-    }
+		articleURLs.concat(postURLs);
+		await _page.close();
+	}
 
-    let validArticleURLs = [];
-    for (const url of articleURLs) {
-        if (url.includes(process.env.MATCH_URL)) {
-            validArticleURLs.push(url);
-        }
-    }
+	if (articleURLs.length == 0) {
+		await browser.close();
+		console.log('Filter returned 0 article URLs for this sitemap URL');
+		return;
+	}
 
-    // TODO Generate proper output file's filename
-    // TODO Save in results folder
+	let validArticleURLs = [];
+	for (const url of articleURLs) {
+		if (url.includes(process.env.MATCH_URL)) {
+			validArticleURLs.push(url);
+		}
+	}
 
-    stringify(validArticleURLs, {
-        header: true, columns: {
-            URL: 'URL',
-        }
-    }, (err, output) => {
-        if (err) throw err;
-        fs.writeFile('my.csv', output, (err) => {
-            if (err) throw err;
-            console.log('my.csv saved.');
-        });
-    });
+	stringify(
+		validArticleURLs,
+		{
+			header: true,
+			columns: {
+				URL: 'URL',
+			},
+		},
+		(err, output) => {
+			if (err) throw err;
+			fs.writeFile(outPutFileName, output, (err) => {
+				if (err) throw err;
+				console.log(`${outPutFileName} saved.`);
+			});
+		}
+	);
 
-    await browser.close();
+	await browser.close();
 })();
